f4e7f18c7910339c97a5ae5c3ffc7e91480aad24
Yolan Maldonado <register@yolan.dev>
2021-05-11 21:51:53 +0200

    commit

---
+++ front/src/routes/index.svelte
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title>PA21</title>
+</head>
+<body>
+    <!-- Navbar
+    - logo
+    - login
+    - signup
+    - if connected -> avatar
+    - if connected -> create a challenge (upload test to pass + a solution file)
+    - switch theme light / dark
+    -->
+
+    <!-- options of editor
+    - share link of exercise
+    - choose language
+    - change color scheme
+    -->
+    <!-- show tests (left of editor)
+    - title of challenge
+    - details of the challenge
+    - input > output example
+    - show test
+    -->
+    <!-- editor (copy code or drop a file)
+     - can write code...
+    -->
+    <!-- editor output / debug bottom left
+     - can filter err / out (redirect stdout / stderr and display them on demand)
+     - can display output
+    -->
+    <!-- actions of editor (bottom)
+    - run test
+    - next
+    -->
+    <!-- output left of debug -->
+    <!-- debug right of output-->
+    <!-- footer -->
+</body>
+</html>
