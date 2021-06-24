7920fbdb3cf01014c27204af2c285863de09f4b6
Yolan Maldonado <register@yolan.dev>
2021-06-25 00:02:36 +0200

    API + UPLOAD FILES

--- sandbox/docker/compile.go
+++ sandbox/docker/compile.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
 )
 
 type language string
@@ -25,9 +30,39 @@ func isLangSupported(givenLanguage language) bool {
 			return true
 		}
 	}
+
 	return false
+}
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+	file, handler, err := r.FormFile("file")
+	fileName := r.FormValue("file_name")
+	if err != nil {
+		panic(err)
+	}
+	defer func(file multipart.File) {
+		err := file.Close()
+		if err != nil {
+			//TODO
+		}
+	}(file)
+
+	f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0770)
+	if err != nil {
+		panic(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			//TODO
+		}
+	}(f)
+	_, _ = io.WriteString(w, "File "+fileName+" Uploaded successfully")
+	_, _ = io.Copy(f, file)
 }
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/compile", UploadFile).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
